b0fdbe7f226eea8f8f8609c86ed8144c79d68e9e
Matt Riese <rise.matt@gmail.com>
2021-05-27 16:29:32 -0700

    added 3 functions

---
+++ src/TodoApp.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({ initialTodos }) {
+  const [todos, setTodos] = useState(initialTodos)
+
+  /** add a new todo to list */
+  function create(newTodo) {
+    let todo = { ...newTodo, id: uuid() }
+    setTodos(todos => [...todos, todo]);
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+    setTodos(todos => todos.map(t => t.id === updatedTodo.id ? updatedTodo : t ));
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+    setTodos(todos => todos.filter(t => t.id !== id));
+  }
+
+  return (
+      <main className="TodoApp">
+        <div className="row">
+
+          <div className="col-md-6">
+            <EditableTodoList /> OR
+            <span className="text-muted">You have no todos.</span>
+          </div>
+
+          <div className="col-md-6">
+            (if no top todo, omit this whole section)
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo />
+            </section>
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              FIXME
+            </section>
+          </div>
+
+        </div>
+      </main>
+  );
+}
+
+export default TodoApp;
---
+++ src/plan.md
@@ -0,0 +1,30 @@
+Components:
+1. Header
+
+2. Todo's box on the left
+
+3. top todo box on the right
+
+
+4. add new todo box
+
+5. footer
+
+6. add/edit form
+props: add todo function
+
+
+state: value of the inputs: title, desc, priority
+
+7. todo.
+ props: edit function
+		delete function
+		title
+		description
+		priority
+state: top/not
+
+8. some parent component with decendents: todolist and toptodo (APP?)
+	state: top todo
+
+	function to pass top todo to top-todo-list and the rest to todo-list
